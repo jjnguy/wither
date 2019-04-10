a92eb1a5de0b20c69b5d0c188b774affc4873484
Justin Nelson <justin.john.nelson@gmail.com>
2019-04-09 21:18:19 -0500

    Add example dithering algorithm

---
+++ Jjnguy.Dither/Jjnguy.Dither/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace Jjnguy.Dither.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("img")]
+        public IActionResult Img()
+        {
+            return File(new HttpClient().GetAsync("https://i.redd.it/k4ro1qrip7r21.jpg").Result.Content.ReadAsByteArrayAsync().Result, "img/jpg");
+        }
+    }
+}
